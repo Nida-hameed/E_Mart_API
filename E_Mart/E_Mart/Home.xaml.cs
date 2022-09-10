f3c61be80f6653aeca1bf842d93d4ba4e0b7c7f2
Nida Hameed <nidahameed87@gmail.com>
2022-09-10 13:39:00 +0500

    Product Detail

--- E_Mart/E_Mart/Home.xaml.cs
+++ E_Mart/E_Mart/Home.xaml.cs
@@ -1,4 +1,7 @@
-﻿using E_Mart.Views.Customer;
+﻿using E_Mart.Models;
+using E_Mart.Utills;
+using E_Mart.Views.Customer;
+using E_Mart.Views.Shop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +16,66 @@ namespace E_Mart
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Home : ContentPage
     {
+        APICall api = new APICall();
         public Home()
         {
-            InitializeComponent(); 
-        } 
+            InitializeComponent();
+            LoadData();
+        }
+        private async void LoadData()
+        {
+
+            try
+            {
+                var responseData = await api.CallApiGetAsync<List<SHOP_tbl>>("api/SHOP_tbl_API/getshops");
+                CollBrand.ItemsSource = responseData; 
+               
+                
+                var responseData1 = await api.CallApiGetAsync<List<PRODUCT_tbl>>("api/PRODUCT_tbl_API/recentproducts");
+                CollNewestProducts.ItemsSource = responseData1; 
+                
+                
+                
+                //var responseData2 = await api.CallApiGetAsync<List<PRODUCT_tbl>>("api/shop/GetCategoryAndProductsByShopId/" );
+                //CollMostSelling.ItemsSource = responseData2;
+               
+
+
+            }
+            catch (Exception ex)
+            {
+
+                await DisplayAlert("Error", "Something went wrong, Please Try Again later.\n Error: " + ex.Message, "OK");
+            }
+        }
+
+
+            private async void CollBrand_SelectionChanged(object sender, SelectionChangedEventArgs e)
+            {
+                await UpdateSelectionDataAsync(e.CurrentSelection);
+            }
+            async Task UpdateSelectionDataAsync(IEnumerable<object> currentSelected)
+            {
+                var selected = currentSelected.FirstOrDefault() as SHOP_tbl;
+                await Navigation.PushAsync(new Products(selected));
+            }
+
+
+            private async void CollNewestProducts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+            {
+                await UpdateSelectionDataAsync2(e.CurrentSelection);
+            }
+
+            private async void CollMostSelling_SelectionChanged(object sender, SelectionChangedEventArgs e)
+            {
+                await UpdateSelectionDataAsync2(e.CurrentSelection);
+            }
+            async Task UpdateSelectionDataAsync2(IEnumerable<object> currentSelected)
+            {
+                var selected = currentSelected.FirstOrDefault() as PRODUCT_tbl;
+                await Navigation.PushAsync(new ProductDetail(selected));
+            }
+
+        
     }
 }
---
+++ E_Mart/E_Mart/Views/Shop/ConfirmOrder.xaml.cs
@@ -0,0 +1,55 @@
+﻿
+using E_Mart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace E_Mart.Shop
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConfirmOrder : ContentPage
+    {
+        public ConfirmOrder()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCurrentLocation_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Models.ORDER_tbl order = new Models.ORDER_tbl()
+                {
+                    ORDER_DATE = DateTime.Now.Date,
+                    ORDER_STATUS = "Booked",
+                    PAYMENT_MODE = "CashOnDelivery",
+                    //SHOP_FID = ,
+                    CUSTOMER_FID= App.LoggedInCustomer.CUSTOMER_ID
+                };
+
+
+
+                foreach (var item in App.Cart)
+                {
+                    
+                    //item.ORDER_FID = response.ORDER_ID;
+                    item.PRO_ORDER_QUANTITY = item.PRO_ORDER_QUANTITY * -1;
+                   
+                }
+
+                //await Navigation.PushAsync(new Success());
+               
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Message", "Somthing went wrong this may be a problem with internet or application please ensure that you have a working internet connection and GPS enabled. \nError Details : " + ex.Message, "OK");
+            }
+        }
+    }
+}
